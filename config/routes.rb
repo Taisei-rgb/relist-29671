605aa165d63624f0c9d755abcc283c2d8ae39ee1
Taisei-rgb <taisei.contact@gmail.com>
2020-09-26 22:07:38 +0900

    細かいwidthの設定など

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'notes/index'
-  resources :notes
+  resources :notes do
+    collection do 
+      get 'search'
+    end
+  end
   root to: "notes#index"
 end
